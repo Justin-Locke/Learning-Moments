6369e718a220f7148d0385ea2ef58bbae794f47d
Justin Locke <Justindeanlocke@gmail.com>
2025-02-19 14:17:53 -0600

    All posts now display post title instead of post body.

--- src/components/posts/Post.jsx
+++ src/components/posts/Post.jsx
@@ -1,7 +1,7 @@
 export const Post = ({ postObj }) => {
     return <section className="post">
     <header className="post-title">
-        <div className="post-info">{postObj.body}</div>
+        <div className="post-info">{postObj.title}</div>
     </header>
     <footer>
         <div className="post-info">
