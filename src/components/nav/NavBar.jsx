efa1d241f80c293664f8b9da6aa1243278c53f88
Justin Locke <Justindeanlocke@gmail.com>
2025-02-19 14:16:00 -0600

    Able to view single post details and return to all posts.

---
+++ src/components/nav/NavBar.jsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import "./NavBar.css"
+
+export const NavBar = () => {
+    const navigate = useNavigate()
+    
+    return <ul className="navbar">
+        <li className="navbar-item">
+            <Link to='/posts'>Posts</Link>
+        </li>
+        <li className="navbar-item">
+            {/* <Link to={'/customers'}>Customers</Link> */}
+        </li>
+        <li className="navbar-item">
+            {/* <Link to={'/employees'}>Employees</Link> */}
+        </li>
+        <li className="navbar-item">
+            {/* <Link to={'/profile'}>Profile</Link> */}
+        </li>
+        {localStorage.getItem("learning_user") ? (
+        <li className="navbar-item navbar-logout">
+            <Link
+            className="navbar-link"
+            to=""
+            onClick={() => {
+                localStorage.removeItem("learning_user")
+                navigate("/", { replace: true })
+            }}
+            >
+            Logout
+            </Link>
+        </li>
+        ) : (
+        ""
+        )}
+        
+    </ul>
+}
--- src/services/postServices.jsx
+++ src/services/postServices.jsx
@@ -1,3 +1,7 @@
 export const getAllPosts = () => {
     return fetch(`http://localhost:8088/posts?_expand=topic&_embed=likes`).then(res => res.json());
 }
+
+export const getPostById = (id) => {
+    return fetch(`http://localhost:8088/posts?id=${id}&_expand=user&_expand=topic&_embed=likes`).then(res => res.json())
+}
